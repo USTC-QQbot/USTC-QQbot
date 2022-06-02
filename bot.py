9beccebe399b93dff6e38993135451b69e476484
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2022-06-02 13:34:19 +0800

    Modify group config

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from random import randrange
 from urllib.parse import quote
 from inspect import currentframe, getframeinfo
 from os.path import isfile
-from json import load, dump
+from json import load, dump, dumps
 
 
 PORT = 4383
@@ -40,6 +40,37 @@ def get_config(func_name: str='', group_id: int=0) -> dict:
         with open(path) as f:
             config.update(load(f).get(func_name, {}))
     return config
+
+
+def set_config(option: str, value, func_name: str='', group_id: int=0):
+    '''Set the config.
+    `group_id`: -1 for base, -2 for override, 0 for auto-retrieve.'''
+    caller = currentframe().f_back
+    if not func_name:
+        func_name = getframeinfo(caller)[2]
+    if not group_id:
+        event = caller.f_locals.get('event')
+        if not event:
+            return
+        if event.detail_type == 'group':
+            group_id = event.group_id
+        else:
+            return
+    if group_id >= 0:
+        path = f"./group_config/{group_id}.json"
+    elif group_id == -1:
+        path = "./config_base.json"
+    elif group_id == -2:
+        path = "./config_override.json"
+    else:
+        return
+    with open(path, encoding="utf-8") as f:
+        config_full = load(f)
+    if not config_full.get(func_name):
+        config_full[func_name] = {}
+    config_full[func_name][option] = value
+    with open(path, "w", encoding="utf-8") as f:
+        dump(config_full, f, indent=4)
 
 
 async def roll(event, msg: Message):
@@ -135,6 +166,28 @@ async def help(event: Event, msg: Message):
     await bot.send(event, "当前可用指令：" + ', '.join(commands))
 
 
+async def config_group(event: Event, msg: Message):
+    '''/config <func> (<option> (<value>))'''
+    cmds = msg_to_txt(msg).split()[1:]
+    if not cmds:
+        return
+    func = group_commands.get(('' if cmds[0].startswith('/') else '/') + cmds[0], None)
+    if not func: return
+    cmds[0] = func.__name__
+    config = get_config(cmds[0])
+    if len(cmds) == 1:
+        await bot.send(event, dumps(config, ensure_ascii=False, indent=4))
+    elif len(cmds) == 2:
+        await bot.send(event, str(config.get(cmds[1])))
+    elif len(cmds) == 3:
+        option, value = cmds[1:]
+        trans = {'true': True, 'false': False}
+        if value.lower() in trans:
+            value = trans[value.lower()]
+        set_config(option, value, func_name=cmds[0])
+        await bot.send(event, "Success.")
+
+
 private_commands = {"/roll": roll, "/time": show_time, "/email": email, "/help": help}
 group_commands = {
     "/roll": roll,
@@ -146,7 +199,7 @@ group_commands = {
     "/help": help
 }
 admin_private_commands = {"/enable": enable, "/disable": disable}
-admin_group_commands = {"/ban": ban, "/enable": enable, "/disable": disable}
+admin_group_commands = {"/ban": ban, "/enable": enable, "/disable": disable, "/config": config_group}
 
 
 @bot.on_message("private")
