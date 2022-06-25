20e0cddb5f3b55b4e26b3e03fd7407c387ed1323
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2022-06-25 23:24:27 +0800

    💈 bug fix

--- bot.py
+++ bot.py
@@ -401,7 +401,7 @@ async def mental(event: Event, msg: Message):
         if qq != 80000000:
             name = (
                 await bot.get_group_member_info(group_id=event.group_id, user_id=qq)
-            )["nickname"]
+            )["card"]
         else:
             name = event.anonymous["name"]
     template = choice(mental_templates)
